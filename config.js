dc8273479bdff28c5180c010d07fcaf76895a497
emely2018-dotcom <emely_2018@mail.ru>
2026-01-03 19:09:22 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+var config = {
+    // Sentences displayed one by one
+    lines: [
+        "Happy 15th Birthday, Gabi! ❤️",
+        "Congrats on surviving another year",
+        "You're officially ancient now (jk).",
+        "I hope you wont die from being that old",
+        "Thanks for being such a good person",
+        "and sometimes listening and enduring my nonsense.",
+        "Hope you eat way too much cake",
+        "and chinese mango",
+        "and get dhiarea",
+        "Stay awesome and be less acoustic pls"
+    ],
+    // Date format: Year-Month-DayTTime
+    // Jan 4th, 2011
+    date: "2011-01-04T00:00:00",
+    // Names at the top
+    names: ["Happy Birthday", "Gabi"]
+}
